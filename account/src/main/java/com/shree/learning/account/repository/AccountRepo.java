cf7f5e31d6d954d1a6f9e4558eaecefa47e815d2
Shree <reachoutshree@gmail.com>
2024-06-04 21:39:53 +0530

    rename of account to account-Service

--- account/src/main/java/com/shree/learning/account/repository/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package com.shree.learning.account.repository;
-
-import com.shree.learning.account.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepo extends JpaRepository<Account, Long> {
-}
